e6d2f42831ce983fa48cefed88b07c4eafb3bc88
Mayur Tamkhane <150355152+MayurT96@users.noreply.github.com>
2026-07-25 19:32:28 +0530

    Time: 499 ms (53.61%), Space: 0B (100%) - LeetHub - Time: O(N), Space: O(1)

---
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select name
+from customer
+WHERE referee_id != 2 OR referee_id IS NULL;
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
